aa96e7f165869da565d7584cbcdf315282ede0de
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2021-05-16 22:52:57 +0800

    Added .env.canary and .env.production configuration file

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,9 +13,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `.env.${
-        process.env.NODE_ENV === 'development' ? 'development' : 'production'
-      }`,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
     TypeOrmModule.forRootAsync({
       useFactory: async () =>

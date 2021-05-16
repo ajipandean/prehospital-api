f6142f9dac45259a39e07998b693ce1352a7f698
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2021-05-16 22:23:23 +0800

    Setup env variable configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,15 @@ import { ChatsGateway } from './chats/chats.gateway';
 import { MessagesModule } from './messages/messages.module';
 import { HospitalsModule } from './hospitals/hospitals.module';
 import { DoctorsModule } from './doctors/doctors.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${
+        process.env.NODE_ENV === 'development' ? 'development' : 'production'
+      }`,
+    }),
     TypeOrmModule.forRootAsync({
       useFactory: async () =>
         Object.assign(await getConnectionOptions(), {
